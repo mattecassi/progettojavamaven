60825966da6cb9ed68785dca0d37f9a0bfda574f
Matteo <matteo.cassinelli.98@gmail.com>
2019-09-10 18:40:06 +0200

    1)Speriamo che sia la volta buona per il db

--- src/main/sql/sqlite.sql
+++ src/main/sql/sqlite.sql
@@ -88,3 +88,50 @@ CREATE TABLE "operazione" (
                             "tipoOperazione"	INTEGER,
                             FOREIGN KEY("idvino") REFERENCES "vino"("ID") on UPDATE CASCADE on delete CASCADE
 );
+CREATE VIEW compitiDaSvolgereQuestaSettimana
+as
+SELECT *
+from compito
+where (
+    (dow is NULL AND dataCompitoOff >= date("now") and dataCompitoOff <= date("now","+" || (7 - strftime("%w","now")) || " days") )
+    or
+    (dow is null and dataCompitoOff is NULL)
+    or
+    (dow = 0 or dow >= strftime("%w", "now"))
+  )
+  AND NOT EXISTS(
+    SELECT *
+    FROM compitoSvolto
+    where idCompito = compito.ID
+      AND
+      (
+
+          (
+                compito.dow = 0 AND
+                dataRisoluzione = date("now","+" || (7 - strftime("%w","now")) || " days")
+            )
+          or
+          (
+                compito.dow > 0 AND
+                dataRisoluzione = date("now","+" || (compito.dow - strftime("%w","now")) || " days")
+            )
+          or
+          (
+              compito.dow is null and compito.dataCompitoOff is not null
+            )
+          or
+          (
+              compito.dow is null and compito.dataCompitoOff is null
+              and dataRisoluzione = date("now")
+            )
+
+        )
+  );
+CREATE VIEW fornitoriEnoteche
+as
+select * from fornitore join enoteca
+where fornitore.ID = enoteca.ID;
+CREATE VIEW fornitoriRappresentanti
+as
+select * from fornitore join rappresentante
+where fornitore.ID = rappresentante.ID;
