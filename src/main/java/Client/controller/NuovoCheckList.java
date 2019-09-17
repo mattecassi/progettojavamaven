c08c40304e06ede9aef0e719e46fc1f16dc0cc9e
bizza251 <bizza251@gmail.com>
2019-09-17 11:57:07 +0200

    Info di tutte le tabelle

--- src/main/java/Client/controller/NuovoCheckList.java
+++ src/main/java/Client/controller/NuovoCheckList.java
@@ -89,7 +89,7 @@ public class NuovoCheckList {
 
     private void closeProcedure() {
         //COSA FACCIO QUANDO CHIUDO
-        checkList.load();
+        //checkList.load();
     }
 
     @FXML
